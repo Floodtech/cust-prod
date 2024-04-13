9aa89d6f48b5b48b688f0cc876a486721ee890b3
alexatwell <alexatwell246@gmail.com>
2024-04-13 18:09:30 -0400

    Commit 3 Add Customer functionality complete

---
+++ src/Customer.java
@@ -0,0 +1,7 @@
+public class Customer {
+    public String businessName;
+    public String contactName;
+    public String customerEmail;
+    public String customerTelNum;
+    public String customerCellNum;
+}
